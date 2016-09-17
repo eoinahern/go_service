f73a3cf74939f197c02fd7c301596d294c1191b0
eoinahern <eoin.ahern@mycit.ie>
2016-09-17 16:41:38 +0100

    updated remote url to db

--- domain/model/database.go
+++ domain/model/database.go
@@ -12,7 +12,10 @@ func NewDatabase(name string, pass string, dbname string) *Database {
 	db := new(Database)
 	var err error
 
-	dbstring := fmt.Sprintf("%s:%s@/%s", name, pass, dbname)
+	//example TCP on a remote host, e.g. Amazon RDS:
+	//id:password@tcp(your-amazonaws-uri.com:3306)/dbname
+
+	dbstring := fmt.Sprintf("%s:%s@tcp(us-cdbr-iron-east-04.cleardb.net:3306)/%s", name, pass, dbname)
 	db.mydbconn, err = sql.Open("mysql", dbstring)
 
 	if err != nil {
